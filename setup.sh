9cbe2e43071d2faafa4de57cc851ca37ff9eb401
lemoer <git@irrelefant.net>
2020-11-10 02:49:35 +0100

    Explicitly use python3 in favor of python in setup.sh

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ SYSTEM_USER=lemoer
 
 cd $DIR
 
-python -m venv venv
-source venv/bin/activate
+python3 -m venv venv
+. venv/bin/activate
 pip install -r requirements.txt
 (cd static; composer install)
 
