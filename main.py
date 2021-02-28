lemoer/keepitup

commit 66714177ff911ad38ec20610a0769df54a5e8eec

Added debug mode for emails.
In debug mode emails will be saved to disk and not send.

main.py
@@ -64,31 +64,35 @@ class User(Base):
         self.send_mail(mail_template, url=url)
 
     def send_mail(self, mail_template, in_reply_to = None, **kwargs):
-        with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
-            server.ehlo()
-            if SMTP_USE_STARTTLS:
-                context = ssl.create_default_context()
-                server.starttls(context=context)
+        msgid = make_msgid()
 
-            msgid = make_msgid()
+        head = []
+        head += ["From: " + SMTP_FROM]
+        head += ["To: " + self.email]
+        head += ["Message-ID: " + msgid]
+        head += ["Reply-To: " + SMTP_REPLY_TO_EMAIL]
+        head += ["Date: " + datetime.datetime.now(pytz.utc).strftime("%a, %e %b %Y %T %z")]
 
-            head = []
-            head += ["From: " + SMTP_FROM]
-            head += ["To: " + self.email]
-            head += ["Message-ID: " + msgid]
-            head += ["Reply-To: " + SMTP_REPLY_TO_EMAIL]
-            head += ["Date: " + datetime.datetime.now(pytz.utc).strftime("%a, %e %b %Y %T %z")]
+        if in_reply_to:
+            head += ["In-Reply-To: " + in_reply_to]
+            head += ["References: " + in_reply_to]
 
-            if in_reply_to:
-                head += ["In-Reply-To: " + in_reply_to]
-                head += ["References: " + in_reply_to]
+        kwargs.update(HEAD="\n".join(head))
+        mail = mail_template.format(**kwargs)
 
-            kwargs.update(HEAD="\n".join(head))
-            mail = mail_template.format(**kwargs)
+        if DEBUG:
+            with open("/tmp/keepitup_mails.log", "a") as f:
+                f.write(mail + "\n")
+        else:
+            with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
+                server.ehlo()
+                if SMTP_USE_STARTTLS:
+                    context = ssl.create_default_context()
+                    server.starttls(context=context)
 
             server.sendmail(SMTP_FROM, self.email, mail)
 
-            return msgid
+        return msgid
 
     @property
     def subscribed_nodes(self):

config.py.example
@@ -30,6 +30,8 @@ FLASK_SECRET_KEY = b'Ia\x19\x00).F\x07\x96V\xca\xea\xd0\xd9i\x16'; raise "REPLAC
 APP_URL = "http://[2a02:790:ff:914:eab1:fcff:fef6:4d16]:8080/"
 
 NODE_LINKS = {
-	"Map": "https://hannover.freifunk.net/karte/#/de/map/{node.nodeid}",
-	"Stats": "https://stats.ffh.zone/d/000000021/router-fur-meshviewer?orgId=1&from=now-7d&to=now-1m&var-node={node.nodeid}"
+    "Map": "https://hannover.freifunk.net/karte/#/de/map/{node.nodeid}",
+    "Stats": "https://stats.ffh.zone/d/000000021/router-fur-meshviewer?orgId=1&from=now-7d&to=now-1m&var-node={node.nodeid}"
 }
+
+DEBUG = False

commit 5a5a54828ca8a7c69bb05bb38880f9b26395ff5d

Fixup: server.sendmail() crashed due to missing connection
Fixes: 66714177ff911ad38ec20610a0769df54a5e8eec

main.py
@@ -90,7 +90,7 @@ class User(Base):
                     context = ssl.create_default_context()
                     server.starttls(context=context)
 
-            server.sendmail(SMTP_FROM, self.email, mail)
+                server.sendmail(SMTP_FROM, self.email, mail)
 
         return msgid
 