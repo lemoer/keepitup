04c6164531ac500304dc8bba19d8037a49546c3e
Leonardo Mörlein <git@irrelefant.net>
2021-02-28 22:48:11 +0100

    Remove influx, ping mechanism and node.ip.

--- ping_worker.py
+++ ping_worker.py
@@ -4,32 +4,17 @@ from main import *
 import time
 
 session = get_session()
-influx = get_influx()
 nodeset = NodeSet()
 
-TIMEOUT = 1
-PING_INTERVAL = 15
-
-influx_keep_time = datetime.timedelta(minutes=60)
-
 nodeset.update_from_db(session)
-nodeset.load_from_influx(influx, delta=influx_keep_time)
 
-print("pinger.py, " + str(len(nodeset.nodes)) + " nodes loaded.")
+print("ping_worker.py, " + str(len(nodeset.nodes)) + " nodes loaded.")
 
 try:
     while True:
         nodeset.update_from_db(session)
 
-        for i in range(PING_INTERVAL // TIMEOUT):
-            start = time.time()
-            nodeset.ping_sliced(i, PING_INTERVAL, TIMEOUT)
-            end = time.time()
-            delta = start + TIMEOUT - end
-            if delta > 0:
-                time.sleep(delta)
-
-        nodeset.save_to_influx(influx)
+        # TODO: Add updating last_seen attribute from nodes.json here...
 
         for n in nodeset.nodes:
             alarm = n.check(session)
@@ -41,7 +26,6 @@ try:
             else:
                 print("node " + n.name + ": alarm")
 
-        nodeset.flush_cache_all(delta=influx_keep_time)
 
 except KeyboardInterrupt:
     print("CTRL + C pressed. Exiting.")
