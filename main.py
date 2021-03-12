lemoer/keepitup

commit a9f474ae95978f349976ec31770f202f94d0013d

Fix unicode issues during mail send
After this commit we properly encode unicode stuff when sending mails.

Before this commit, things broke like this:

    Feb 21 01:15:24 harvester python[2332]: Traceback (most recent call last):
    Feb 21 01:15:24 harvester python[2332]:   File "/opt/keepitup/ping_worker.py", line 35, in <module>
    Feb 21 01:15:24 harvester python[2332]:     alarm = n.check(session)
    Feb 21 01:15:24 harvester python[2332]:   File "/opt/keepitup/main.py", line 446, in check
    Feb 21 01:15:24 harvester python[2332]:     alarm.send_notification_mails(session)
    Feb 21 01:15:24 harvester python[2332]:   File "/opt/keepitup/main.py", line 284, in send_notification_mails
    Feb 21 01:15:24 harvester python[2332]:     self.alarm_mail_msgid = user.send_mail(mail_template, node=node, url=url)
    Feb 21 01:15:24 harvester python[2332]:   File "/opt/keepitup/main.py", line 89, in send_mail
    Feb 21 01:15:24 harvester python[2332]:     server.sendmail(SMTP_FROM, self.email, mail)
    Feb 21 01:15:24 harvester python[2332]:   File "/usr/lib/python3.7/smtplib.py", line 855, in sendmail
    Feb 21 01:15:24 harvester python[2332]:     msg = _fix_eols(msg).encode('ascii')
    Feb 20 19:02:45 harvester python[2332]: UnicodeEncodeError: 'ascii' codec can't encode character '\xfc' in position 265: ordinal not in range(128)

Fixes #2

main.py
@@ -13,6 +13,10 @@ import datetime
 import requests
 import smtplib, ssl
 from email.utils import make_msgid
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+from email.charset import Charset, QP
 
 from sqlalchemy import create_engine, func, event
 from sqlalchemy.ext.declarative import declarative_base
@@ -66,19 +70,31 @@ class User(Base):
     def send_mail(self, mail_template, in_reply_to = None, **kwargs):
         msgid = make_msgid()
 
-        head = []
-        head += ["From: " + SMTP_FROM]
-        head += ["To: " + self.email]
-        head += ["Message-ID: " + msgid]
-        head += ["Reply-To: " + SMTP_REPLY_TO_EMAIL]
-        head += ["Date: " + datetime.datetime.now(pytz.utc).strftime("%a, %e %b %Y %T %z")]
+        # Default encoding mode set to Quoted Printable. Acts globally!
+        #Charset.add_charset('utf-8', Charset.QP, Charset.QP, 'utf-8')
+
+        subject = mail_template['subject'].format(**kwargs)
+        message = mail_template['message'].format(**kwargs)
+
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = str(Header(subject, 'utf-8'))
+        msg['From'] = str(Header(SMTP_FROM, 'utf-8'))
+        msg['To'] = str(Header(self.email, 'utf-8'))
+        msg['Message-ID'] = msgid
+        msg['Reply-To'] = SMTP_REPLY_TO_EMAIL
+        msg['Date'] = datetime.datetime.now(pytz.utc).strftime("%a, %e %b %Y %T %z")
 
         if in_reply_to:
-            head += ["In-Reply-To: " + in_reply_to]
-            head += ["References: " + in_reply_to]
+            msg['In-Reply-To'] = in_reply_to
+            msg['References'] = in_reply_to
 
-        kwargs.update(HEAD="\n".join(head))
-        mail = mail_template.format(**kwargs)
+        # add message
+        charset = Charset('utf-8')
+        # QP = quoted printable; this is better readable instead of base64, when
+        # the mail is read in plaintext!
+        charset.body_encoding = QP
+        message_part = MIMEText(message.encode('utf-8'), 'plain', charset)
+        msg.attach(message_part)
 
         if DEBUG:
             with open("/tmp/keepitup_mails.log", "a") as f:
@@ -90,7 +106,7 @@ class User(Base):
                     context = ssl.create_default_context()
                     server.starttls(context=context)
 
-                server.sendmail(SMTP_FROM, self.email, mail)
+                server.sendmail(SMTP_FROM, self.email, msg.as_string())
 
         return msgid
 

mail_templates.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-CONFIRM = """{HEAD}
-Subject: Confirm your email for KeepItUp! :)
-
-Hi there,
+CONFIRM = dict(
+    subject="Confirm your email for KeepItUp! :)",
+    message="""Hi there,
 
 you need to confirm your email with this token:
 
@@ -18,12 +17,11 @@ please kindly ignore this mail.
 
 Kind regards,
 lemoer
-"""
+""")
 
-ALARM = """{HEAD}
-Subject: [KeepItUp] alarm: {node.name} is unreachable
-
-Hi there,
+ALARM = dict(
+    subject="[KeepItUp] alarm: {node.name} is unreachable",
+    message="""Hi there,
 
 the node {node.name} is unreachable.
 
@@ -31,12 +29,11 @@ Link to node: {url}
 
 Kind regards,
 lemoer
-"""
+""")
 
-RESOLVED = """{HEAD}
-Subject: [KeepItUp] resolved: {node.name} is reachable again
-
-Hi there,
+RESOLVED = dict(
+    subject="[KeepItUp] resolved: {node.name} is reachable again",
+    message="""Hi there,
 
 the node {node.name} is now reachable again.
 
@@ -45,3 +42,4 @@ Link to node: {url}
 Kind regards,
 lemoer
 """
+)

commit 9b1c90e9c233dfde76c65e30e7d0347380326df6

Fix DEBUG mails crash
Fixes: a9f474a ("Fix unicode issues during mail send")

main.py
@@ -95,7 +95,7 @@ class User(Base):
 
         if DEBUG:
             with open("/tmp/keepitup_mails.log", "a") as f:
-                f.write(mail + "\n")
+                f.write(msg.as_string() + "\n")
         else:
             with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
                 server.ehlo()